c62e27b6b154b466bc0679d30366a02fbe58085e
aruantec <aruantec@gmail.com>
2026-02-07 23:52:56 +0100

    Added MainMenuView & CachedContentPresenter

---
+++ AES_Lacrima/ViewModels/Navigation/MainMenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AES_Core.DI;
+using AES_Core.Interfaces;
+
+namespace AES_Lacrima.ViewModels.Navigation
+{
+    public interface IMainMenuViewModel : IViewModelBase;
+
+    [AutoRegister]
+    internal partial class MainMenuViewModel : ViewModelBase, IMainMenuViewModel
+    {
+
+    }
+}
