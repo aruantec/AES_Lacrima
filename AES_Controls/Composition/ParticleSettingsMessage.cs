dc465133a7cfec1d49b2dc1beaf725d7fe189bd0
aruantec <aruantec@gmail.com>
2026-02-07 18:41:20 +0100

    Added ParticleCompositionControl

---
+++ AES_Controls/Composition/ParticleSettingsMessage.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Media;
+using Avalonia.Media.Imaging;
+
+namespace AES_Controls.Composition;
+
+/// <summary>
+/// A message containing settings for the particle visual.
+/// </summary>
+internal class ParticleSettingsMessage
+{
+    public int ParticleCount;
+    public Bitmap? Background;
+    public Stretch Stretch;
+    public bool IsPaused;
+}
