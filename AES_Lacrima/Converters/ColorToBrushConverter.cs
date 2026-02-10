5f5eb134d3d907122357c9279fe675c3908b1f7e
aruantec <aruantec@gmail.com>
2026-02-10 14:14:13 +0100

    Added media controls

--- AES_Lacrima/Converters/ColorToBrushConverter.cs
+++ AES_Lacrima/Converters/ColorToBrushConverter.cs
@@ -5,8 +5,22 @@ using System.Globalization;
 
 namespace AES_Lacrima.Converters;
 
+/// <summary>
+/// Converter that maps an <see cref="Avalonia.Media.Color"/> to a
+/// <see cref="SolidColorBrush"/> and vice versa.
+/// Useful for binding color values to controls that expect a <see cref="Brush"/>.
+/// </summary>
 public class ColorToBrushConverter : IValueConverter
 {
+    /// <summary>
+    /// Converts a <see cref="Color"/> to a <see cref="SolidColorBrush"/>.
+    /// If the input is not a <see cref="Color"/>, <c>null</c> is returned.
+    /// </summary>
+    /// <param name="value">The source value. Expected to be a <see cref="Color"/>.</param>
+    /// <param name="targetType">The target binding type (ignored).</param>
+    /// <param name="parameter">Optional parameter (ignored).</param>
+    /// <param name="culture">The culture to use in the converter (ignored).</param>
+    /// <returns>A new <see cref="SolidColorBrush"/> when <paramref name="value"/> is a <see cref="Color"/>, otherwise <c>null</c>.</returns>
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is Color color)
@@ -14,6 +28,15 @@ public class ColorToBrushConverter : IValueConverter
         return null;
     }
 
+    /// <summary>
+    /// Converts a <see cref="SolidColorBrush"/> back to its <see cref="Color"/>.
+    /// If the input is not a <see cref="SolidColorBrush"/>, <c>null</c> is returned.
+    /// </summary>
+    /// <param name="value">The source value. Expected to be a <see cref="SolidColorBrush"/>.</param>
+    /// <param name="targetType">The target binding type (ignored).</param>
+    /// <param name="parameter">Optional parameter (ignored).</param>
+    /// <param name="culture">The culture to use in the converter (ignored).</param>
+    /// <returns>The <see cref="Color"/> extracted from the brush when possible; otherwise <c>null</c>.</returns>
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is SolidColorBrush brush)
