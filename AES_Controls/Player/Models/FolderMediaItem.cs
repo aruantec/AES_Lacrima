bd2aecb0df20b11b0effe5edb688e44aeaa31f09
aruantec <aruantec@gmail.com>
2026-02-11 00:28:47 +0100

    Added FolderMediaItem for album interactions, added xml descriptions

---
+++ AES_Controls/Player/Models/FolderMediaItem.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Collections;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AES_Controls.Player.Models
+{
+    /// <summary>
+    /// Represents a specialized media item that acts as a container for other media items,
+    /// such as a folder or an album directory.
+    /// </summary>
+    public partial class FolderMediaItem : MediaItem
+    {
+        /// <summary>
+        /// Gets or sets the list of child media items contained within this folder.
+        /// </summary>
+        [ObservableProperty]
+        private AvaloniaList<MediaItem> _children = [];
+    }
+}
