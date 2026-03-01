0e994460fb731b28f9cc81ce31b94e6c8fdc8dd6
Joel Fung <aruantec@gmail.com>
2026-03-01 13:28:42 +0100

    Added BrushToColorConverter
    Added converter to avoid issues when compiling in macOS

---
+++ AES_Lacrima/Converters/BrushToColorConverter.cs
@@ -0,0 +1,30 @@
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace AES_Lacrima.Converters
+{
+    public class BrushToColorConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is SolidColorBrush solidColorBrush)
+            {
+                return solidColorBrush.Color;
+            }
+            return Colors.Transparent; // Or a default color
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is Color color)
+            {
+                return new SolidColorBrush(color);
+            }
+            return null;
+        }
+    }
+}
+
+
