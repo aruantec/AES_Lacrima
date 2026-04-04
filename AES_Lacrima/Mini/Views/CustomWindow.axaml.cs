3651b7f06a4ca2284c6c58d05737819206b02814
aruantec <aruantec@gmail.com>
2026-04-05 00:07:35 +0200

    Implement media key handling in CustomWindow and MainWindow for improved user interaction

--- AES_Lacrima/Mini/Views/CustomWindow.axaml.cs
+++ AES_Lacrima/Mini/Views/CustomWindow.axaml.cs
@@ -1,5 +1,6 @@
 using AES_Core.DI;
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace AES_Lacrima.Mini.Views;
 
@@ -9,5 +10,36 @@ public partial class CustomWindow : Window
     {
         InitializeComponent();
         DataContext ??= DiLocator.TryResolve(typeof(ViewModels.MinViewModel));
+        KeyDown += OnKeyDown;
+    }
+
+    private void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e == null)
+            return;
+
+        if (DataContext is not ViewModels.MinViewModel vm)
+            return;
+
+        switch (e.Key)
+        {
+            case Key.MediaNextTrack:
+                if (vm.NextCommand.CanExecute(null))
+                    vm.NextCommand.Execute(null);
+                e.Handled = true;
+                break;
+
+            case Key.MediaPreviousTrack:
+                if (vm.PreviousCommand.CanExecute(null))
+                    vm.PreviousCommand.Execute(null);
+                e.Handled = true;
+                break;
+
+            case Key.MediaPlayPause:
+                if (vm.PlayPauseCommand.CanExecute(null))
+                    vm.PlayPauseCommand.Execute(null);
+                e.Handled = true;
+                break;
+        }
     }
 }
